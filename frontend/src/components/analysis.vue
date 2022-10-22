a7cb996a3c8d5039be820b5ac60654c4ae0275df
tom.mansion <tomansion@yahoo.fr>
2022-10-22 16:45:29 +0200

    vis network init

--- frontend/src/components/analysis.vue
+++ frontend/src/components/analysis.vue
@@ -2,11 +2,14 @@
   <div>
     Hello
     {{analysedBlueprint}}
+    <div id="mynetwork"></div>
   </div>
 </template>
 
 <script>
 import { analysisStore } from '@/stores/analysis'
+import { Network } from "vis-network";
+import { DataSet } from "vis-data";
 
 export default {
   name: 'analysis-page',
@@ -18,16 +21,41 @@ export default {
   mounted() {
     this.analysedBlueprint = analysisStore.analysedBlueprint
 
-    console.log(analysisStore);
-    console.log(this.analysedBlueprint);
-
     if (this.analysedBlueprint === undefined) {
       this.$router.push({ name: 'Home' })
     }
+    console.log(Network);
+    console.log(DataSet);
+
+    var nodes = new DataSet([
+      { id: 1, label: "Node 1" },
+      { id: 2, label: "Node 2" },
+      { id: 3, label: "Node 3" },
+      { id: 4, label: "Node 4" },
+      { id: 5, label: "Node 5" },
+    ]);
+
+    var edges = new DataSet([
+      { from: 1, to: 3 },
+      { from: 1, to: 2 },
+      { from: 2, to: 4 },
+      { from: 2, to: 5 },
+      { from: 3, to: 3 },
+    ]);
+
+    var options = {};
+
+    // create a network
+    var container = document.getElementById("mynetwork");
+    var network = new Network(container, { nodes, edges }, options);
   }
 }
 </script>
 
 <style>
-
+#mynetwork {
+  width: 600px;
+  height: 400px;
+  border: 1px solid lightgray;
+}
 </style>
